b7ee33b814bd15b4110d36b51bf11219a3667bcb
Rajendhiran E <rajendhiran.easu@liveramp.com>
2026-01-10 12:05:09 +0530

    practice langchain

---
+++ GodPracticeWithLLM.py
@@ -0,0 +1,21 @@
+"""
+Author: Rajendhiran Easu
+Date: 10/01/26
+Description: 
+"""
+
+from langchain_ollama import ChatOllama
+
+llm = ChatOllama(model="llama3.2:latest", temperature=0.7)
+result = llm.invoke("Tell me something on learning langchain? in 1 simple line")
+# result = llm.batch(["simple tips on learning in 1 line", "langchain?", "langraph?"])
+# result = llm.stream(["simple tips on learning in 10 words", "langchain?"])
+
+print(result.content)
+
+# for msg in result:
+#     print(msg.content, end="\n\n")
+#     #print(msg.model_dump_json(indent=2),end="\n\n", flush=True)
+
+# for msg in result:
+#     print(msg.content, end="", flush=True)
