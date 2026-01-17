dbfd4d1924b5314f96e056d7331d72cf42c536b0
Rajendhiran E <rajendhiran.easu@liveramp.com>
2026-01-17 23:20:29 +0530

    init_chat_model

--- GodPracticeWithLLM.py
+++ GodPracticeWithLLM.py
@@ -5,8 +5,11 @@ Description:
 """
 
 from langchain_ollama import ChatOllama
+from langchain.chat_models import init_chat_model
 
-llm = ChatOllama(model="llama3.2:latest", temperature=0.7)
+llm = init_chat_model(model="llama3.2:latest", model_provider="ollama", temperature=0.7)
+
+#llm = ChatOllama(model="llama3.2:latest", temperature=0.7)
 result = llm.invoke("Tell me something on learning langchain? in 1 simple line")
 # result = llm.batch(["simple tips on learning in 1 line", "langchain?", "langraph?"])
 # result = llm.stream(["simple tips on learning in 10 words", "langchain?"])
