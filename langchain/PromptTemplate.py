6550cf36a7e61806e5d6d5ac30def7bef804585b
Rajendhiran E <rajendhiran.easu@liveramp.com>
2026-05-08 17:08:26 +0530

    prompt template with respective message types

--- langchain/PromptTemplate.py
+++ langchain/PromptTemplate.py
@@ -4,6 +4,7 @@ Date: 10/01/26
 Description: 
 """
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts.chat import AIMessagePromptTemplate, HumanMessagePromptTemplate
 from langchain_ollama import ChatOllama
 
 model = ChatOllama(model="llama3.2:latest", temperature=0.7)
@@ -11,21 +12,25 @@ model = ChatOllama(model="llama3.2:latest", temperature=0.7)
 # Prompt Template
 
 # prompt = ChatPromptTemplate.from_template("Tell me a joke on {user_input}?")
+# prompt = ChatPromptTemplate.from_messages([
+#     ("ai", "generate a mock details in the prescribed {format}"),
+#     ("user", "provide a details on {instruction}")
+# ])
+
 prompt = ChatPromptTemplate.from_messages([
-    ("ai", "generate a mock details in the prescribed {format}"),
-    ("user", "provide a details on {instruction}")
+    AIMessagePromptTemplate.from_template("generate a mock details in the prescribed {format}"),
+    HumanMessagePromptTemplate.from_template("provide a details on {instruction}")
 ])
 
 # Check prompt value
 # prompt_value = prompt.invoke({"format": "json",
-#                          "instruction": "3 to 5 simple employee details who are working in different dept. in the factory"})
+#                         "instruction": "3 to 5 simple student details who are studying in different dept. in the college"})
 # print(prompt_value)
+# response = model.invoke(prompt_value)
+# print(response.content)
 
 # Constructing the chain (LCEL)
 chain = prompt | model
-
-# response = chain.invoke({"user_input":"doctor and patient"})
-
 response = chain.invoke({"format": "json",
                          "instruction": "3 to 5 simple employee details who are working in different dept. in the factory"})
 print(response.content)
