9cbae43ef2dfcbb0a8909817a6ebfa459d252550
Rajendhiran E <rajendhiran.easu@liveramp.com>
2026-05-08 18:20:56 +0530

    explored the explicit message types

--- langchain/GodPracticeWithLLM.py
+++ langchain/GodPracticeWithLLM.py
@@ -6,11 +6,17 @@ Description:
 
 from langchain_ollama import ChatOllama
 from langchain.chat_models import init_chat_model
+from langchain.messages import AIMessage, HumanMessage
 
 llm = init_chat_model(model="llama3.2:latest", model_provider="ollama", temperature=0.7)
 
 #llm = ChatOllama(model="llama3.2:latest", temperature=0.7)
-result = llm.invoke("Tell me something on learning langchain? in 1 simple line")
+#result = llm.invoke("Tell me something on learning langchain? in 1 simple line")
+result = llm.invoke([
+    AIMessage("generate a mock details in the prescribed xml formant"),
+    HumanMessage("provide a details on 3 to 5 simple student details who are studying in different dept. in the college")
+])
+
 # result = llm.batch(["simple tips on learning in 1 line", "langchain?", "langraph?"])
 # result = llm.stream(["simple tips on learning in 10 words", "langchain?"])
 
@@ -20,5 +26,6 @@ print(result.content)
 #     print(msg.content, end="\n\n")
 #     #print(msg.model_dump_json(indent=2),end="\n\n", flush=True)
 
+## Stream
 # for msg in result:
 #     print(msg.content, end="", flush=True)
